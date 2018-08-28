6e966cc5e8d6474729c9c420efae6e522de7a8d9
leviboenish <leviboenish@gmail.com>
2018-08-28 14:45:37 -0600

    books component now holds api call for future routing

--- src/App.js
+++ src/App.js
@@ -5,27 +5,13 @@ import TopNav from './components/TopNav.js';
 import Books from './components/Books.js';
 
 class App extends Component {
-  constructor(props){
-      super(props)
-        this.state = {
-          data: null
-        }
-    }
-
-    componentDidMount() {
-      let apiURL = 'https://boe-two-day-app.herokuapp.com/books';
-      fetch(apiURL)
-        .then(response => response.json())
-        .then(response =>
-          this.setState({ data: response }));
-    }
 
   render() {
     return (
       <div className="App">
         <Header />
         <TopNav />
-        <Books books={this.state.data} />
+        <Books />
       </div>
     );
   }
--- src/components/Books.js
+++ src/components/Books.js
@@ -1,35 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
-const Books = (props) => {
-if(props.books !== null){
-  console.log(props.books)
-  let bookArr = props.books.data.map((item, i) => {
-    console.log(item)
+class Books extends Component {
+  constructor(props){
+      super(props)
+        this.state = {
+          data: null
+        }
+    }
+
+    componentDidMount() {
+      let apiURL = 'https://boe-two-day-app.herokuapp.com/books/';
+      fetch(apiURL)
+        .then(response => response.json())
+        .then(response =>
+          this.setState({ data: response.data }));
+    }
+
+render(){
+  if(this.state.data !== null){
+    let bookArr = this.state.data.map((item, i) => {
+      return (
+        <li className='book' key={i}>
+          <img className='book-cover' src={item.BookCoverURL} />
+          <h4  className='book-title'>Title: {item.BookTitle}</h4>
+          <h5  className='book-genre'>Genre: {item.BookGenre}</h5>
+          <p className='book-description'>{item.BookDescription}</p>
+        </li>
+      )
+    })
+
     return (
-      <li className='book' key={i}>
-        <img className='book-cover' src={item.BookCoverURL} />
-        <h4  className='book-title'>Title: {item.BookTitle}</h4>
-        <h5  className='book-genre'>Genre: {item.BookGenre}</h5>
-        <p className='book-description'>{item.BookDescription}</p>
-      </li>
+      <section>
+        <ul>
+          {bookArr}
+        </ul>
+      </section>
+      );
+  } else {
+    return(
+      <div>
+        <p>Loading</p>
+      </div>
     )
-  })
-
-  return (
-    <section>
-      <ul>
-        {bookArr}
-      </ul>
-    </section>
-    );
-} else {
-  return(
-    <div>
-      <p>Loading</p>
-    </div>
-  )
+  }
 }
-
 }
 export default Books;
